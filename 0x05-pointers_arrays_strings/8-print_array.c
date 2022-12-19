1a43b8f9df6c51944b3efed77c06132187348547
Egwono <ekarieren@gmail.com>
2022-12-19 17:28:26 +0100

    correction

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -6,16 +6,16 @@
  *   * @a: input array.
  *    * @n: input n elements
  *     * Return: no return.
- *      */
+ */
 void print_array(int *a, int n)
 {
-		int i = 0;
+	int i = 0;
 
-			for (; i < n; i++)
-					{
-								printf("%d", *(a + i));
-										if (i != (n - 1))
-														printf(", ");
-											}
-				printf("\n");
+	for (; i < n; i++)
+	{
+		printf("%d", *(a + i));
+		if (i != (n - 1))
+			printf(", ");
+	}
+	printf("\n");
 }
