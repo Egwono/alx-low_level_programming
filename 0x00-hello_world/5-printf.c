6e122517a382463bc6fe4f20ee4c6bd9b0381fd5
Egwono <ekarieren@gmail.com>
2023-09-07 14:01:06 -0700

    ewwww

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,14 @@
-include <stdio.h>
+#include <stdio.h>
+
 /*
- *  * main - entry of the program
- *   *
- *    * Description - prints a message using the printf function
- *     *
- *      *Return: Always 0 (Success)
- *      */
+ * main - Entry point of the program
+ *
+ * Description: Prints a message with proper grammar using printf
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
+	printf("%s", "with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
